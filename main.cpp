8191e16763ccc4888121d47ec38902f02a7dd178
Ethan Hardey <55114546+HardeyEthan@users.noreply.github.com>
2024-02-17 21:52:04 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -20,23 +20,11 @@ using namespace std;
 int main(int argc, char* const argv[]) {
 
     //Create the index
-    LinearHashIndex emp_index("EmployeeIndex.dat");
+    LinearHashIndex emp_index("EmployeeIndex.bin");
     emp_index.createFromFile("Employee.csv");
 
-    // ofstream testFile;
-    // testFile.open("TEST.dat", ios::in | ios::ate);
-    // testFile.write("BRUH", 4);
-    // testFile.seekp(50);
-    // testFile.write("BRO!", 4);
-    // testFile.close();
+
     
-    // testFile.open("TEST.dat", ios::in | ios::ate);
-    // testFile.seekp(7);
-    // testFile.write("TEMP", 4);
-    // testFile.close();
-
-
-    /*
     char userChoice = ' ';
     int userId = 0;
 
@@ -44,13 +32,13 @@ int main(int argc, char* const argv[]) {
     {
       cout << "Please enter a Employee ID to search: ";
       cin >> userId;
-      manager.findRecordById(userId);
+      emp_index.findRecordById(userId);
 
       cout << "Do you want to search again[y,n]: ";
       cin >> userChoice;
       userChoice = tolower(userChoice);
     }
-    */
+    
 
     return 0;
 }
