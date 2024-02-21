6761c5d916fbe0b54bb4c56ade92e9f889f66d48
Ethan Hardey <55114546+HardeyEthan@users.noreply.github.com>
2024-02-20 18:17:00 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -49,47 +49,4 @@ int main(int argc, char* const argv[]) {
     
 
     return 0;
-}/*
-Skeleton code for linear hash indexing
-*/
-
-#include <string>
-#include <ios>
-#include <fstream>
-#include <vector>
-#include <string>
-#include <string.h>
-#include <iostream>
-#include <sstream>
-#include <stdexcept>
-#include <cmath>
-#include "classes.h"
-
-using namespace std;
-
-
-int main(int argc, char* const argv[]) {
-
-    //Create the index
-    LinearHashIndex emp_index("EmployeeIndex.bin");
-    emp_index.createFromFile("Employee.csv");
-
-
-    
-    char userChoice = ' ';
-    int userId = 0;
-
-    while(userChoice != 'n')
-    {
-      cout << "Please enter a Employee ID to search: ";
-      cin >> userId;
-      emp_index.findRecordById(userId);
-
-      cout << "Do you want to search again[y,n]: ";
-      cin >> userChoice;
-      userChoice = tolower(userChoice);
-    }
-    
-
-    return 0;
 }
